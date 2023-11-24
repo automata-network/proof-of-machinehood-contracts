8416b321682e1faab31be10ff8475d5bc8da00dd
Preston Ong <preston4896@me.com>
2023-11-24 22:38:03 +0800

    deploy script as functions

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
 source .env
+source ./script/script.sh
 
-VERIFY_COMMAND=""
-
-for (( i=1; i<=$#; i++ )); do
-    if [ "${!i}" = "--verify" ]
-    then
-        VERIFY_COMMAND="--verify --verifier blockscout --verifier-url ${VERIFIER_URL}"
-        continue
-    fi
-done
-
-forge script script/Deploy.sol --sig "deployAndroidSafetyNet()" --broadcast --rpc-url ${RPC_URL} $VERIFY_COMMAND | grep LOG
-forge script script/Deploy.sol --sig "deployWindowsTPM()" --broadcast --rpc-url ${RPC_URL} $VERIFY_COMMAND | grep LOG
-forge script script/Deploy.sol --sig "deployYubikey()" --broadcast --rpc-url ${RPC_URL} $VERIFY_COMMAND | grep LOG
+deploy
