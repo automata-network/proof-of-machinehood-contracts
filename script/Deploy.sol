e2ab064514f74dde0078b45e8357512acb0e61d4
Preston Ong <preston4896@me.com>
2023-11-24 18:08:07 +0800

    temp: use .env to load lib addresses for now

--- script/Deploy.sol
+++ script/Deploy.sol
@@ -4,27 +4,33 @@ pragma solidity ^0.8.0;
 import "../src/AndroidSafetyNet.sol";
 import "../src/WindowsTPM.sol";
 import "../src/Yubikey.sol";
-import "./deployment/LibScript.sol";
 
-contract Deploy is LibScript {
+import "forge-std/Script.sol";
+
+// use .env addresses for now
+// import "./deployment/LibScript.sol";
+
+contract Deploy is Script {
+
+    uint256 internal privateKey = vm.envUint("PRIVATE_KEY");
+    address sigVerifyLib = vm.envAddress("SIG_VERIFY_LIB");
+    address derParser = vm.envAddress("DER_PARSER");
+
     function deployAndroidSafetyNet() public {
-        vm.broadcast(privateKey);
-        Lib memory lib = read();
-        new AndroidSafetyNet(lib.sigVerifyLib, lib.derParser);
+        vm.startBroadcast(privateKey);
+        new AndroidSafetyNet(sigVerifyLib, derParser);
         vm.stopBroadcast();
     }
 
     function deployWindowsTPM() public {
-        vm.broadcast(privateKey);
-        Lib memory lib = read();
-        new WindowsTPM(lib.sigVerifyLib, lib.derParser);
+        vm.startBroadcast(privateKey);
+        new WindowsTPM(sigVerifyLib, derParser);
         vm.stopBroadcast();
     }
 
     function deployYubikey() public {
-        vm.broadcast(privateKey);
-        Lib memory lib = read();
-        new Yubikey(lib.sigVerifyLib, lib.derParser);
+        vm.startBroadcast(privateKey);
+        new Yubikey(sigVerifyLib, derParser);
         vm.stopBroadcast();
     }
 }
