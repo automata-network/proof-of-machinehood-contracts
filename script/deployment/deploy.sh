3531d34a706ae1d27cc13ddeb3fb51871a4cf2c7
Preston Ong <preston@ata.network>
2023-11-24 22:38:47 +0800

    script patch: deploy script as functions (#7)

--- script/deployment/deploy.sh
+++ script/deployment/deploy.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
 source .env
+source ./script/deployment/script.sh
 
-VERIFY_COMMAND=""
-
-for (( i=1; i<=$#; i++ )); do
-    if [ "${!i}" = "--verify" ]
-    then
-        VERIFY_COMMAND="--verify --verifier blockscout --verifier-url ${VERIFIER_URL}"
-        continue
-    fi
-done
-
-forge script script/deployment/LibScript.sol --sig "deployLib()" --broadcast --rpc-url ${RPC_URL} $VERIFY_COMMAND | grep LOG
+deploy
---
+++ script/deployment/script.sh
@@ -0,0 +1,16 @@
+# script.sh
+
+deploy()
+{
+    VERIFY_COMMAND=""
+
+    for (( i=1; i<=$#; i++ )); do
+        if [ "${!i}" = "--verify" ]
+        then
+            VERIFY_COMMAND="--verify --verifier blockscout --verifier-url ${VERIFIER_URL}"
+            continue
+        fi
+    done
+
+    forge script script/deployment/LibScript.sol --sig "deployLib()" --broadcast --rpc-url ${RPC_URL} $VERIFY_COMMAND | grep LOG
+}
